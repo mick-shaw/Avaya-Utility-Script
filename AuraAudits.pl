53ce147a0c2ad0400681ee6c218bab3c84050b37
Mick Shaw <mickjshaw@gmail.com>
2014-11-19 20:17:45 -0500

    Added menu contruct

--- AuraAudits.pl
+++ AuraAudits.pl
@@ -29,8 +29,8 @@ my $debug ='';
 my $help =0;
 my $node;
 my $phone;
-my $output;
-my $printout;
+my $choice;
+my $emailaddresses;
 
 ###########################################################
 #
@@ -70,6 +70,15 @@ my $PBXListStation_CoveragePath	= 		'004fff00';
 my $PBXDisplayStation_CoveragePath = 	'8007ff00';
 my $PBXDisplayStation_VoiceMailButton = '801f063d';
 
+our %CMD_FN_MAP =(
+MENU_MAIN => \&MENU_MAIN, #
+
+MENU_DISC_OPT => \&MENU_DISC_OPT, #
+MENU_IPENDPT_OPT => \&MENU_IPENDPT_OPT, #
+
+RUN_DISC_REPORT => \&FN_RUN_DISC_REPORT, #
+RUN_IPENDPT_REPORT => \&FN_RUN_IPENDPT_REPORT, #
+);
 
 ###########################################################
 
@@ -93,16 +102,16 @@ sub getDisconnectedEndpoints
 						
 					if ($hash_ref->{$PBXStatusStation_ServiceState} eq 'disconnected' or $hash_ref->{$PBXStatusStation_ServiceState} eq 'out-of-service')
 					{
-					
-					
+										
 					$Single_DisconnectedEndpoint = ($hash_ref->{$PBXStatusStation_Extension}.",".$hash_ref->{$PBXStatusStation_Port}.",".$hash_ref->{$PBXStatusStation_ProgrammedType}.",".$hash_ref->{$PBXStatusStation_ServiceState}."\n");
 					push (@DisconnectedEndpoints, $Single_DisconnectedEndpoint);
 					
 					}
-			}	
+			}
+	
+	return @DisconnectedEndpoints;
 
-} return @DisconnectedEndpoints
-
+} 
 
 sub getListStations
 {
@@ -138,9 +147,53 @@ $node->do_logoff();
 
 }
 
+sub MENU_MAIN {
+print "\n Aura Audit Report Menu \n";
+print "1. Disconnect Report\n";
+print "2. IP-Endpoint Report\n";
+print 'Your Audit Report choice ? ';
+chomp($choice = <STDIN>);
+
+return 'MENU_DISC_OPT' if $choice == 1;
+return 'MENU_IPENDPT_OPT' if $choice == 2;
+return '';
+}
+
+sub MENU_DISC_OPT {
+print "\n Disconnect Report\n";
+print 'Email addresses: ';
+chomp($emailaddresses = <STDIN>);
+
+return 'RUN_DISC_REPORT'
+}
+
+sub FN_RUN_DISC_REPORT
+{
+print "\nYour Disconnect report is running \n";
 runDisconnectReport();
+return '';
+}
 
+sub MENU_IPENDPT_OPT {
+print "\n IP-Endpoint Report\n";
+print 'Email addresses: ';
+chomp($emailaddresses = <STDIN>);
 
+return 'RUN_IPENDPT_REPORT'
+}
+
+sub FN_RUN_IPENDPT_REPORT
+{
+print "\nYour IP-Endpoint report is running \n";
+return '';
+}
+my $next = MENU_MAIN();
+while (1)
+{
+exit if !$next;
+die if !exists $CMD_FN_MAP{uc($next)};
+$next = &{$CMD_FN_MAP{uc($next)}}();
+} 
 
 
 
